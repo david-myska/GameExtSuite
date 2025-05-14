45f9a3a2cb3a29122dae36351e44b6dcf38fada5
David Myska <david.myska@thermofisher.com>
2025-05-14 11:56:57 +0200

    Countess, missing gold stuff

--- test/ge_tests/d2/achievements/act1/countess_gold_steal.h
+++ test/ge_tests/d2/achievements/act1/countess_gold_steal.h
@@ -6,22 +6,42 @@ namespace D2::Achi::CountessGoldSteal
 {
     struct CD
     {
+        std::optional<Data::GUID> m_countessId;
+        uint32_t m_initialGold = 0;
     };
 
     auto Create()
     {
-        return BLD<CD>("ClearTristram")
-            .Add(GE::ConditionType::Precondition, "In Tristram",
+        return BLD<CD>("Fort Boyard")
+            .Add(GE::ConditionType::Activator, "Enter Bloodthrone",
                  [](const D2::Data::DataAccess& aDataAccess, const D2::Data::SharedData& aS, CD& aC) {
-                     return aDataAccess.GetMisc().GetZone() == Data::Zone::Act1_Tristram;
+                     return aDataAccess.GetMisc().GetZone() == Data::Zone::Act1_Bloodthrone;
                  })
-            .Add(GE::ConditionType::Activator, "Enter Tristram",
+            .OnPass(GE::ConditionType::Activator,
+                    [](const D2::Data::DataAccess& aDataAccess, const D2::Data::SharedData& aS, CD& aC) {
+                        aC.m_initialGold = 0;  // TODO
+                    })
+            .Add(GE::ConditionType::Completer, "Kill The Countess",
                  [](const D2::Data::DataAccess& aDataAccess, const D2::Data::SharedData& aS, CD& aC) {
-                     return true;
+                     if (!aC.m_countessId)
+                     {
+                         Data::GUID countessId = 0;
+                         if (MonsterNearby("THE COUNTESS", aDataAccess, countessId))
+                         {
+                             aC.m_countessId = countessId;
+                         }
+                         return false;
+                     }
+                     return aS.GetDeadMonsters().contains(*aC.m_countessId);
                  })
-            .Add(GE::ConditionType::Completer, "Kill all monsters in Tristram",
+            .Add(GE::ConditionType::Failer, "Stay in Bloodthrone",
                  [](const D2::Data::DataAccess& aDataAccess, const D2::Data::SharedData& aS, CD& aC) {
-                     return false;
+                     return aDataAccess.GetMisc().GetZone() != Data::Zone::Act1_Bloodthrone;
+                 })
+            .Add(GE::ConditionType::Validator, "Gold collected",
+                 [](const D2::Data::DataAccess& aDataAccess, const D2::Data::SharedData& aS, CD& aC) {
+                     int32_t goldCollected = 0;  // aDataAccess.GetPlayers().GetLocal().m_stats... - aC.m_initialGold;
+                     return goldCollected >= 1000;
                  })
             .Build();
     }
